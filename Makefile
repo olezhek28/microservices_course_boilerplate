58dc522e3fa50cc6799f63d6251c1d607f782bdb
d.bogdanov <bogdanov.dmitriy@rambler.ru>
2024-04-24 22:20:17 +0300

    add week_2

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 LOCAL_BIN:=$(CURDIR)/bin
 
+LOCAL_MIGRATION_DIR=$(MIGRATION_DIR)
+LOCAL_MIGRATION_DSN="host=localhost port=$(PG_PORT) dbname=$(PG_DATABASE_NAME) user=$(PG_USER) password=$(PG_PASSWORD) sslmode=disable"
+
 install-golangci-lint:
 	GOBIN=$(LOCAL_BIN) go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.53.3
 
@@ -9,6 +12,7 @@ lint:
 install-deps:
 	GOBIN=$(LOCAL_BIN) go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.1
 	GOBIN=$(LOCAL_BIN) go install -mod=mod google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+	GOBIN=$(LOCAL_BIN) go install github.com/pressly/goose/v3/cmd/goose@v3.14.0
 
 get-deps:
 	go get -u google.golang.org/protobuf/cmd/protoc-gen-go
@@ -26,3 +30,12 @@ generate-auth-api:
 	--go-grpc_out=pkg/auth_v1 --go-grpc_opt=paths=source_relative \
 	--plugin=protoc-gen-go-grpc=bin/protoc-gen-go-grpc \
 	api/auth_v1/auth.proto
+
+local-migration-status:
+	${LOCAL_BIN}/goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} status -v
+
+local-migration-up:
+	${LOCAL_BIN}/goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} up -v
+
+local-migration-down:
+	${LOCAL_BIN}/goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} down -v
--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -2,26 +2,55 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"flag"
+	sq "github.com/Masterminds/squirrel"
 	"github.com/brianvoe/gofakeit"
+	"github.com/dmtrybogdanov/auth/internal/config"
+	"github.com/dmtrybogdanov/auth/internal/config/env"
+	desc "github.com/dmtrybogdanov/auth/pkg/auth_v1"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"log"
 	"net"
-
-	desc "github.com/dmtrybogdanov/auth/pkg/auth_v1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
-const grpcPort = 50051
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "config-path", ".env", "path to config file")
+}
 
 type server struct {
 	desc.UnimplementedAuthV1Server
+	pool *pgxpool.Pool
 }
 
 func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
 	log.Printf("create name: %s", req.Name)
+
+	builderSelectOne := sq.Select("id").
+		From("auth").
+		PlaceholderFormat(sq.Dollar).
+		Where(sq.Eq{"name": req.Name}).
+		Limit(1)
+
+	query, args, err := builderSelectOne.ToSql()
+	if err != nil {
+		log.Fatalf("failed to build query: %v", err)
+	}
+
+	var id int64
+	var name string
+
+	err = s.pool.QueryRow(ctx, query, args...).Scan(&id, &name)
+	if err != nil {
+		log.Fatalf("failed to select notes: %v", err)
+	}
+
+	log.Printf("id: %d, usernames: %s: %v\n", id, name)
 
 	return &desc.CreateResponse{
 		Id: gofakeit.Int64(),
@@ -53,14 +82,38 @@ func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.
 }
 
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	flag.Parse()
+	ctx := context.Background()
+
+	err := config.Load(configPath)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	grpcConfig, err := env.NewGRPCConfig()
+	if err != nil {
+		log.Fatalf("failed to get grpc config: %v", err)
+	}
+
+	pgConfig, err := env.NewPGConfig()
+	if err != nil {
+		log.Fatalf("failed to get pg config: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", grpcConfig.Address())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	pool, err := pgxpool.Connect(ctx, pgConfig.DSN())
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer pool.Close()
+
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterAuthV1Server(s, &server{})
+	desc.RegisterAuthV1Server(s, &server{pool: pool})
 
 	log.Printf("server listening at %v", lis.Addr())
 
