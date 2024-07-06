797de58f8f932f5c5cfb5615fbf4e896cabbc836
Ivan Semeniv <neracastle@gmail.com>
2024-07-06 09:03:45 +0300

    мелкий рефакторинг

--- internal/grpc-server/server.go
+++ internal/grpc-server/server.go
@@ -23,19 +23,21 @@ func (s *Server) Get(ctx context.Context, req *userdesc.GetRequest) (*userdesc.G
 	log.Printf("called Get method with req: %v", req)
 
 	return &userdesc.GetResponse{
-		Id: req.GetId(),
-		User: &userdesc.User{
-			Name:  gofakeit.BeerName(),
-			Email: gofakeit.Email(),
-			Role:  userdesc.Role_USER,
-		},
+		Id:        req.GetId(),
+		Name:      gofakeit.BeerName(),
+		Email:     gofakeit.Email(),
+		Role:      userdesc.Role_USER,
 		CreatedAt: timestamppb.New(gofakeit.Date()),
 		UpdatedAt: timestamppb.New(gofakeit.Date()),
 	}, nil
 }
 
 func (s *Server) Update(ctx context.Context, req *userdesc.UpdateRequest) (*emptypb.Empty, error) {
-	log.Printf("called Update method with req: %v", req)
+	if req.Role == nil {
+		log.Printf("called Update method with empty role: %v", req)
+	} else {
+		log.Printf("called Update method with setted role: %v", req)
+	}
 
 	return &emptypb.Empty{}, nil
 }
