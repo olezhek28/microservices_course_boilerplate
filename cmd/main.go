db6d7b2792a2f6ca37b9f8fe1def93e2d0d14bfd
egorb <egor.belyaev95@yandex.ru>
2024-07-17 20:18:39 +0300

    1 homework is completed

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"time"
 
+	desc "github.com/BelyaevEI/microservices_client/pkg/auth_v1"
 	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	address = "localhost:50051"
+	noteID  = 12
 )
 
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to server: %v", err)
+	}
+
+	defer conn.Close()
+
+	c := desc.NewAuthV1Client(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Get(ctx, &desc.GetRequest{Id: noteID})
+	if err != nil {
+		log.Fatalf("failed to get note by id: %v", err)
+	}
+
+	log.Printf(color.RedString("Note info:\n"), color.GreenString("%+v", r.GetUser()))
+
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,15 @@
-module github.com/olezhek28/microservices_course_boilerplate
+module github.com/BelyaevEI/microservices_client
 
-go 1.20
+go 1.22.2
 
 require (
-	github.com/fatih/color v1.15.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
 )
