7ec4ca75db401aee9e0bd8b89d6c50cabde7ccd9
algol <algol.py@gmail.com>
2024-10-04 11:48:48 +0300

    added protobuf declaration and grpc server

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,78 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
+	"time"
 
-	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	desc "github.com/algol-84/auth/pkg/auth_v1"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedAuthV1Server
+}
+
+// Create User
+func (s *server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Create new user: %+v with role %v", req.Info, req.Info.Role)
+
+	return &desc.CreateResponse{
+		Id: 0,
+	}, nil
+}
+
+// Get User info by ID
+func (s *server) Get(_ context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Get user by ID: %d", req.GetId())
+
+	return &desc.GetResponse{
+		User: &desc.GetUser{
+			Id:        req.GetId(),
+			Name:      "Noname",
+			Email:     "empty@mail.ru",
+			Role:      desc.Role_USER,
+			CreatedAt: timestamppb.New(time.Now()),
+			UpdatedAt: timestamppb.New(time.Now()),
+		},
+	}, nil
+}
+
+// Update User info
+func (s *server) Update(_ context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("Update user by ID: %d", req.Id)
+
+	return &emptypb.Empty{}, nil
+}
+
+// Delete User by ID
+func (s *server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Delete user by ID: %d", req.Id)
+
+	return &emptypb.Empty{}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterAuthV1Server(s, &server{})
+
+	log.Printf("auth server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,16 @@
-module github.com/olezhek28/microservices_course_boilerplate
+module github.com/algol-84/auth
 
-go 1.22
-
-require github.com/fatih/color v1.17.0
+go 1.23
 
 require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/golang/protobuf v1.5.4
+	google.golang.org/grpc v1.67.1
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
 )
